4b7c487a69fee1c948ab6900a6f4ba4b3815a649
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-03-16 18:08:46 +0600

    update pin matcher

--- pin-matcher/style.js
+++ pin-matcher/style.js
@@ -1,17 +1,16 @@
-
+// This function generate pin with size 4
 function getPin() {
-    const pin = Math.round(Math.random() * 10000);
-    const pinString = pin + "";
-    if (pinString.length == 4) {
-        return pin;
-    }
-    else {
-        return getPin();
-    }
+  const pin = Math.round(Math.random() * 10000);
+  const pinString = pin + "";
+  if (pinString.length == 4) {
+    return pin;
+  } else {
+    return getPin();
+  }
 }
 
-
+// This function show the generated pin in the UI
 function pinGenerator() {
-    const pin = getPin();
-    document.getElementById("display-pin").value = pin;
+  const pin = getPin();
+  document.getElementById("display-pin").value = pin;
 }
